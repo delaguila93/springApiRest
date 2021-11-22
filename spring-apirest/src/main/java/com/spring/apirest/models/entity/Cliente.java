ae51ede49e37e7a2a0e443264821ac86bb50b1d9
delaguila93 <josedelaguila93@gmail.com>
2021-11-22 09:52:51 +0100

    Añadido metodo para que la fecha al crearla por defecto no sea null

--- spring-apirest/src/main/java/com/spring/apirest/models/entity/Cliente.java
+++ spring-apirest/src/main/java/com/spring/apirest/models/entity/Cliente.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -31,6 +32,10 @@ public class Cliente implements Serializable {
 	@CreatedDate
 	private Date createdAt;
 	
+	@PrePersist
+	public void prePersist() {
+		createdAt = new Date();
+	}
 	
 	public long getId() {
 		return id;
